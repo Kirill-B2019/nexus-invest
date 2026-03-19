2513cb862f786d8133c5d85f8125cd4b5b002c2a
Kirill-B2019 <57626806+Kirill-B2019@users.noreply.github.com>
2026-03-19 14:38:00 +0300

    style(guest): adjust copyright text alignment
    - Modify class settings for copyright text container
    - Ensure consistent alignment across different screen sizes

--- resources/views/layouts/guest/footer.blade.php
+++ resources/views/layouts/guest/footer.blade.php
@@ -197,7 +197,7 @@
             <div class="row align-items-center mb-20 pb-3" style="border-bottom: 1px solid rgba(255,255,255,0.08);">
                 {{-- РАЗГРАНИЧЕНИЕ ПРАВОВОГО СТАТУСА: ГАНИМЕД И ПЛАТФОРМА НЕКСУС --}}
                 <div class="col-12 mb-20 mb-md-0 text-center text-sm-start">
-                    <div  class="alert alert-footer">
+
                         <p class="neutral-300 uppercase text-md">{{ __('РАЗГРАНИЧЕНИЕ ПРАВОВОГО СТАТУСА: ГАНИМЕД И ПЛАТФОРМА НЕКСУС') }}</p>
                         <p class="neutral-600 mb-0 small">
                             {{ __('ГАНИМЕД предоставляет технологическую инфраструктуру, которую
@@ -218,8 +218,7 @@
                                    России в специальный реестр, в информационной системе которого
                                    осуществляются выпуск, обращение и учёт ЦФА.') }}
                         </p>
-                    </div>
-                </div>
+                                </div>
 
 
 
